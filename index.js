258ecae91704d340b8359409d1d9148b17f90038
LoTwT <709937065@qq.com>
2021-07-03 19:12:41 +0800

    pug init

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const port = 3333
 
 const server = app.listen(port, () => console.log(`server is running at http://localhost:${port}`))
 
+// 指定模版引擎
+app.set("view engine", "pug")
+app.set("views", "views")
+
 app.get("/", (request, response, next) => {
-    response.send("server is running!")
+    response.status(200).render("Home")
 })
