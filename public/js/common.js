292fc552dde7104d96b19372634d7c0f01ab706f
LoTwT <709937065@qq.com>
2021-07-05 12:54:37 +0800

    post content style

--- public/js/common.js
+++ public/js/common.js
@@ -13,7 +13,8 @@ $("#postTextarea").keyup(event => {
     submitButton.prop("disabled", false)
 })
 
-$("#submitPostButton").click(() => {
+$("#submitPostButton").click((event) => {
+    const button = $(event.target)
     const textbox = $("#postTextarea")
     const data = {
         content: textbox.val()
@@ -22,6 +23,49 @@ $("#submitPostButton").click(() => {
     // 发起请求
     // /api/posts => http://localhost:3333/api/posts
     $.post("/api/posts", data, (postData, status, xhr) => {
-        console.log(postData)
+        const html = createPostInfoHtml(postData)
+        $(".postsContainer").prepend(html)
+        textbox.val("")
+        button.prop("disabled", true)
     })
 })
+
+function createPostInfoHtml(postData) {
+    const postedBy = postData.postedBy
+    return `
+        <div class="post">
+            <div class="mainContentContainer">
+                <div class="userImageContainer">
+                    <img src="${postedBy.avatar}" alt="">
+                </div>
+                <div class="postContentContainer">
+                    <div class="header">
+                        <a href="/profile/${postedBy.username}" class="displayName">${postedBy.name}</a>
+                        <span class="username">@${postedBy.username}</span>
+                        <span class="date">${postedBy.createdAt}</span>
+                    </div>
+                    <div class="postBody">
+                        <span>${postData.content}</span>
+                    </div>
+                    <div class="postFooter">
+                        <div class="postButtonContainer">
+                            <button>
+                                <i class="fa fa-comment"></i>
+                            </button>
+                        </div>
+                        <div class="postButtonContainer">
+                            <button>
+                                <i class="fa fa-retweet"></i>
+                            </button>
+                        </div>
+                        <div class="postButtonContainer">
+                            <button>
+                                <i class="fa fa-heart"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `
+}
